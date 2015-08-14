f83a1cc719beddee7fbc14642dd3682ab2283055
busybigwhite <busybigwhite@gmail.com>
2015-08-14 12:09:43 +0800

    table less modefied

--- public/build/bundle/realtime-index.js
+++ public/build/bundle/realtime-index.js
@@ -22596,10 +22596,10 @@ exports.renderTableList = function(infos) {
       	<div class="table-col"><%= info.mold_id %></div>
       	<div class="table-col"><%= info.customer_id %></div>
       	<div class="table-col-sm"><%= info.target_num %></div>
-      	<div class="table-col-sm"><%= info.start_date %></div>
+      	<div class="table-col"><%= info.start_date %></div>
       	<div class="table-col-sm"><%= info.current_fail_num %>/<%= info.current_num %></div>
       	<div class="table-col-sm"><%= info.abnormal_num %></div>
-      	<div class="table-col">
+      	<div class="table-col-lg">
       		<button class="realtime-showpic-btn" data-type="current" data-info=<%= info.workorder_id %>/<%= info.machine_id %>/<%= info.mold_id %>>即時</button>
         	<button class="realtime-showpic-btn" data-type="safety" data-info=<%= info.workorder_id %>/<%= info.machine_id %>/<%= info.mold_id %>>安全</button>
         	<button class="realtime-showpic-btn" data-type="error" data-info=<%= info.workorder_id %>/<%= info.machine_id %>/<%= info.mold_id %>>異常</button>
