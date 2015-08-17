be1018d71730d562a2bbbca638c63ae9aff686c8
TimGo <tim_yang@moremote.com>
2015-08-17 16:46:34 +0800

    基本樣式，header 還沒好的版本

---
+++ public/scripts/pages/workorder-index.js
@@ -0,0 +1,13 @@
+'use strict';
+var $ = window.jQuery = require('jquery');
+var header = require('../includes/header');
+
+
+function initialize() {
+	header.include();
+	bindEvents();
+}
+
+function bindEvents() {
+
+}
