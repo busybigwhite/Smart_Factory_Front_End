40971452e6f0a971b1b878e20cb56ef50b8c242e
nktx <nkt.unknown@gmail.com>
2015-08-19 13:35:00 +0800

    Filter out admin from member list

--- public/scripts/member/template.js
+++ public/scripts/member/template.js
@@ -7,16 +7,21 @@ exports = module.exports = {};
 exports.render = function(infos) {
 
   var menuTemp = _.template(
-	 `<% _.forEach(infos, function(info) {  %>
-      <li class="table-item">
+	 `<% _.forEach(infos, function(info) {
+      if (info.group === 'Administrator') {
+
+      } else { %>
+        <li class="table-item">
           <div class="table-col"><%= info.name %></div>
           <div class="table-col"><%= info.email %></div>
           <div class="table-col"><%= info.group %></div>`+
           `<div class="table-col">
               <button class="member-edit-button" data-id="<%= info.id %>">編輯</button>
           </div>
-      </li>
-    <% }); %>`
+        </li>
+     <% };
+      
+     }); %>`
 	);
 
   return menuTemp(infos);
