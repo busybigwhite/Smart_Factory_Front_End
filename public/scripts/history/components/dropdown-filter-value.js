23d6fd85ecd7142fc22e6c20bbb630d1fb115687
busybigwhite <busybigwhite@gmail.com>
2015-08-21 17:25:54 +0800

    bug fixed

--- public/scripts/history/components/dropdown-filter-value.js
+++ public/scripts/history/components/dropdown-filter-value.js
@@ -72,7 +72,7 @@ function renderDropdown(type) {
 
 	resetBlock();
 
-	var filterType = _.trim(type, '_').replace('id', 's');
+	var filterType = type.replace('_', '').replace('id', 's');
 
 	if( filterValues[filterType].length ){
 	 	var filters = _.uniq( filterValues[filterType] );
