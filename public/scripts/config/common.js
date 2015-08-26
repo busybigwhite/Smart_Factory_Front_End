3caff4c8139feaffc5c5c59611da4c7c7ed9ea59
busybigwhite <busybigwhite@gmail.com>
2015-08-26 13:42:38 +0800

    add CONFIG

---
+++ public/scripts/config/common.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * common.js
+ *
+ * @author  Bigwhite Tseng
+ *
+ */
+exports = module.exports = {};
+
+exports.realtimeReloadTime = 300000;
+
+exports.currentPicReloadTime = 60000;
